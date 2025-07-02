78a3c666fa7c67ebfc662ed82b77c5e0ab5d1644
Vaibhav Bhatambrekar <vbhatambrekar@mondee.com>
2025-07-02 21:13:00 +0530

    updated swagger title

--- factory.py
+++ factory.py
@@ -11,7 +11,7 @@ def create_app():
     if not os.getenv('ENVIRONMENT', None):
         dotenv_path = 'core/.env'
         load_dotenv(dotenv_path)
-    app = FastAPI(title="Ride Share API Documentation",
+    app = FastAPI(title="Ride Share Users API Documentation",
                   openapi_url="/api/openapi.json",  # Custom URL for the OpenAPI schema
                   docs_url="/api/docs",
                   redoc_url="/api/re-docs")
